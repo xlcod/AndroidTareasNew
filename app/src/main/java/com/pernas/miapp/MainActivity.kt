f9fbeeea73b6a9fc679abb949ac2c826ff584caa
xlcod <alvaro.pernas@live.u-tad.com>
2019-10-07 10:03:31 +0200

    first

---
+++ app/src/main/java/com/pernas/miapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.pernas.miapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
