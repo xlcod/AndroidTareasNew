7ae768045cf94229e81d6edfa6eb24ea3253fbb1
Alvaro <xlcodfree@gmail.com>
2019-10-12 19:15:49 +0200

    added intents for  the Profile activity

--- app/src/main/java/com/pernas/miapp/MainActivity.kt
+++ app/src/main/java/com/pernas/miapp/MainActivity.kt
@@ -8,41 +8,37 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var profileButton : Button
+    private lateinit var profileButton: Button
+    private lateinit var button: Button
+    private lateinit var myTextView: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button = findViewById<Button>(R.id.myButton)// pick the id
-        var myTextView = findViewById<TextView>(R.id.textView) // pick the id
-        var sum = 0
+        button = findViewById<Button>(R.id.myButton)
+        myTextView = findViewById<TextView>(R.id.textView)
         profileButton = findViewById(R.id.myProfile)
-
-
+        var sum = 0
 
         profileButton.setOnClickListener {
-        val intent = Intent(this, ProfileActivity::class.java)
-        intent.putExtra("name","Brandom semenuk")
-            intent.putExtra("birth_date"," 15/15/92")
-            intent.putExtra("city","Alberta")
-            intent.putExtra("image",R.drawable.foto)
-
+            val intent = Intent(this, ProfileActivity::class.java)
+            intent.putExtra("riderName", "Brandom semenuk")
+            intent.putExtra("birth_date", " 15/15/92")
+            intent.putExtra("city", "Alberta")
+            intent.putExtra("image", R.drawable.foto)
 
             startActivity(intent)
 
-
         }
-
         button.setOnClickListener() {
             sum += 1
-            var message = getString(R.string.button_pressed,sum,sum)
+            var message = getString(R.string.button_pressed, sum, sum)
             myTextView.setText("""$message  $sum veces""")//concadeno la variable que va sumando
             //myTextView.text= message
         }
     }
-
-
 }
 
 
--- app/src/main/java/com/pernas/miapp/ProfileActivity.kt
+++ app/src/main/java/com/pernas/miapp/ProfileActivity.kt
@@ -2,12 +2,50 @@ package com.pernas.miapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class ProfileActivity : AppCompatActivity() {
+
+
+    private lateinit  var nameRider: TextView
+    private lateinit  var birthDate: TextView
+    private lateinit  var city: TextView
+    private lateinit  var riderImage: ImageView
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-       val name =  intent.extras?.getString("name")
+       //val name =  intent.extras?.getString("name")
+
+        nameRider = findViewById(R.id.nameRider)
+        birthDate = findViewById(R.id.BirthDate)
+        city = findViewById(R.id.city)
+        riderImage = findViewById(R.id.riderImage)
+
+        var intent = intent
+
+        var name_rider = intent.extras?.getString("riderName")
+        var birth_date = intent.extras?.getString("birth_date")
+        var cityName = intent.extras?.getString("city")
+        var profileImage = intent.getIntExtra("image",1)
+
+        nameRider.text = name_rider
+        birthDate.text = birth_date
+        city.text = cityName
+        riderImage.setImageResource(profileImage)
+
+
+
+
+
+
+
+
+
+
     }
 }
